0530ceba9c9fb15d3a4ce5ded731fbe9dbff11cc
Fedemartorana <federico.martorana@outlook.it>
2025-07-26 18:23:09 +0200

    Update works.js

--- works/works.js
+++ works/works.js
@@ -12,11 +12,11 @@ const dateTimeSpan = document.querySelector('.date-time');
 
 // Lista progetti (sostituisci con i tuoi dati reali)
 const projects = [
-    { title: "Miesian House", image: "img/miesianhouse.jpg" },
-    { title: "Hypogeum", image: "img/hypogeum.jpg" },
-    { title: "House Atelier", image: "img/houseatelier.jpg" },
-    { title: "Archive, Exhibit, Inhabit", image: "img/archiveexhibitinhabit.jpg" },
-    { title: "Tetra", image: "img/tetra.jpg" }
+    { title: "Miesian House", image: "/img/miesianhouse.jpg" },
+    { title: "Hypogeum", image: "/img/hypogeum.jpg" },
+    { title: "House Atelier", image: "/img/houseatelier.jpg" },
+    { title: "Archive, Exhibit, Inhabit", image: "/img/archiveexhibitinhabit.jpg" },
+    { title: "Tetra", image: "/img/tetra.jpg" }
 ];
 
 // Genera quadrati progetto
